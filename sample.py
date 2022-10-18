5bc796fa7bfc21906241c1faae9093d64e4637cd
Phoenixhitthu <iamhitthu007@gmail.com>
2022-10-18 14:18:47 +0530

    updated

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 a=10
 b=5
 temp=a
@@ -6,12 +6,4 @@ a=b
 b=temp
 print('value of a is: ',a)
 print('value of b is: ',b)
-=======
-x=10
-y=5
-temp=x
-x=y
-y=temp
-print('value of x is: ',x)
-print('value of y is: ',y)
->>>>>>> 714a5aebce242b46325d71ee63241d84c2a36ead
+
